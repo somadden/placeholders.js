f546e8bb0b03f3c4bce7f4e5e41d070ab381a5c2
Sean Madden <somadden@gmail.com>
2013-04-25 15:40:48 -0700

    Made CSS and browser version automatic

--- placeholders.js
+++ placeholders.js
@@ -1,30 +1,55 @@
 /* 
- * Adds placeholder tag functionality to IE 9 or less
- * version 1.0 By Sean Madden 
- * License : Use it anyway you want;
- * 
- * In the <head> or CSS file add,
- * CSS  : <style>* .auto-pholder-css{color:#a3a3a3 !important;}</style>
- * 
- * Near the bottom of the page add,
- * HTML : <!--[if lte IE 9 ]><script>placeholders();</script><![endif]--> 
+ * Adds placeholder tag functionality to IE 9 and IE 8
+ * Add this file above the </body> tag
+ * version 1.1 By Sean Madden 
  */  
 
-function placeholders(){
-
+(function(){
+	
 	var placeHolderCssName = 'auto-pholder-css';
+	var color = '#a3a3a3';
 
-	for (var i=0; i<document.getElementsByTagName('input').length; i++) {
-		var curNode = document.getElementsByTagName('input')[i];
-		var type = curNode.getAttribute("type").toLowerCase();
-		if(type != "radio" && type != 'checkbox' && type != 'submit'){
-			if (curNode.value == "") {
-				useHTML5Placeholders(curNode);
-				if (curNode.value != "") {
-					valueSave(curNode);
+	var ieVers = (function(){
+ 		var undef,
+			v = 3,
+			div = document.createElement('div'),
+			all = div.getElementsByTagName('i');
+		while (
+			div.innerHTML = '<!--[if gt IE ' + (++v) + ']><i></i><![endif]-->',
+			all[0]
+		);
+		return v > 4 ? v : undef;
+ 
+	}());
+		
+	if(ieVers > -1 && ieVers < 10){
+		addCss();
+		for (var i=0; i<document.getElementsByTagName('input').length; i++) {
+			var curNode = document.getElementsByTagName('input')[i];
+			var type = curNode.getAttribute("type").toLowerCase();
+			if(type != "radio" && type != 'checkbox' && type != 'submit'){
+				if (curNode.value == "") {
+					useHTML5Placeholders(curNode);
+					if (curNode.value != "") {
+						valueSave(curNode);
+					}
 				}
 			}
 		}
+	}
+	
+	function addCss() {
+		var css = '.'+placeHolderCssName+'{color:'+color+' !important;',
+		head = document.getElementsByTagName('head')[0],
+		style = document.createElement('style');
+
+		style.type = 'text/css';
+		if (style.styleSheet){
+		  style.styleSheet.cssText = css;
+		} else {
+		  style.appendChild(document.createTextNode(css));
+		}
+		head.appendChild(style);
 	}
 
 	function useHTML5Placeholders(curNode){
@@ -48,17 +73,15 @@ function placeholders(){
 		if(this.value === this.tmpVal) {
 			this.value = "";
 		}
-
 		var regEx = RegExp("(?:^|\\s)" + placeHolderCssName + "(?!\\S)","g");
 		this.className = this.className.replace(regEx,'');
-
 	}
 
 	function unclicked() {
 		if(this.value === "") {
 			this.value = this.tmpVal;
 			this.className += " " + placeHolderCssName;
-
 		}
 	}
-}
+
+})()
